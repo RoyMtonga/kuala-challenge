44aaa14e4cb125736bc62f435a8e9c1c615be411
Roy Mtonga <mtongaroy@gmail.com>
2024-10-29 10:19:41 +0200

    fsd

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -58,7 +58,7 @@ export function DataTable<TData, TValue>({
     return (
         <>
             <div className="rounded-md border">
-                <div className="flex items-center py-4">
+                <div className="flex items-center py-4 pl-4">
                     <Input
                         placeholder={`Search by ${searchKey}...`}
                         value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
@@ -95,7 +95,7 @@ export function DataTable<TData, TValue>({
                                     data-state={row.getIsSelected() && "selected"}
                                 >
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
+                                        <TableCell key={cell.id} className="pl-10">
                                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     ))}
